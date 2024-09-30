151bcfd6db129d11430f55b7d7bb979f6060ada5
Jules Delannoy <jules.delannoy@etu.utc.fr>
2024-09-30 17:35:05 +0200

    Add SpringBoot

---
+++ SpringBoot/SpringBoot/src/main/java/AI13/SpringBoot/Application.java
@@ -0,0 +1,13 @@
+package AI13.SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
