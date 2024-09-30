3732ecc1612ddff9c224ac9db67aad3195e186e1
ibenyago <imene.benyagoub@etu.utc.fr>
2024-09-30 18:31:00 +0200

    models/user

---
+++ SpringBoot/SpringBoot/src/main/java/AI13/SpringBoot/models/User.java
@@ -0,0 +1,110 @@
+package AI13.SpringBoot.models;
+
+import java.sql.Timestamp;
+
+public class User {
+
+    private int id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    private String company;
+    private String phone;
+    private Timestamp creationDate;
+    private Boolean isActive;
+
+    // Constructeur par défaut
+    public User() {
+    }
+
+    // Getters et Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Timestamp getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Timestamp creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Boolean getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(Boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    // Méthode toString pour afficher l'objet
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", company='" + company + '\'' +
+                ", phone='" + phone + '\'' +
+                ", creationDate=" + creationDate +
+                ", isActive=" + isActive +
+                '}';
+    }
+}
