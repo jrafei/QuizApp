1a0fe78b23ee8789b2ce7ac6a3c8ea3b98c48111
ibenyago <imene.benyagoub@etu.utc.fr>
2024-11-26 21:06:58 +0100

    commit temporaire

--- backend/src/main/java/com/quizapp/quizApp/repository/UserRepository.java
+++ backend/src/main/java/com/quizapp/quizApp/repository/UserRepository.java
@@ -11,5 +11,5 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-    Optional<User> findByEmail(String email); // Vérification d'unicité
+    Optional<User> findByEmail(String email);
 }
